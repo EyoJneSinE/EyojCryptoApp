5e1929d61359fb828490459606575232ebf14043
Enis <enis.d.kaner@gmail.com>
2023-06-12 20:10:00 +0300

    CyrptoListScreen added

---
+++ app/src/main/java/com/eniskaner/eyojcryptoapp/view/CyrptoListScreen.kt
@@ -0,0 +1,10 @@
+package com.eniskaner.eyojcryptoapp.view
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun CyrptoListScreen(navController: NavController) {
+    Text(text = "hello world")
+}
