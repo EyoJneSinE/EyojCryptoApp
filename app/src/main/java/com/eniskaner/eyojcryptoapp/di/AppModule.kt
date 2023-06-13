b159a23b083870f7f85d04cca146b048b0156b7c
Enis <enis.d.kaner@gmail.com>
2023-06-13 13:04:59 +0300

    CyrptoAPI bind with retrofit in AppModule

--- app/src/main/java/com/eniskaner/eyojcryptoapp/di/AppModule.kt
+++ app/src/main/java/com/eniskaner/eyojcryptoapp/di/AppModule.kt
@@ -1,11 +1,26 @@
 package com.eniskaner.eyojcryptoapp.di
 
+import com.eniskaner.eyojcryptoapp.service.CyrptoAPI
+import com.eniskaner.eyojcryptoapp.util.Constatnts.API_URL
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
 
+    @Singleton
+    @Provides
+    fun provideCyrptoApi() : CyrptoAPI {
+        return Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(API_URL)
+            .build()
+            .create(CyrptoAPI::class.java)
+    }
 }
