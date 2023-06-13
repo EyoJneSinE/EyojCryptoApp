ed004989c5e2e8e03bf465e4865840b32d016ddb
Enis <enis.d.kaner@gmail.com>
2023-06-13 12:32:44 +0300

    get func added on CyrptoAPI interface

--- app/src/main/java/com/eniskaner/eyojcryptoapp/di/AppModule.kt
+++ app/src/main/java/com/eniskaner/eyojcryptoapp/di/AppModule.kt
@@ -8,5 +8,4 @@ import dagger.hilt.components.SingletonComponent
 @InstallIn(SingletonComponent::class)
 object AppModule {
 
-    
 }
---
+++ app/src/main/java/com/eniskaner/eyojcryptoapp/service/CyrptoAPI.kt
@@ -0,0 +1,12 @@
+package com.eniskaner.eyojcryptoapp.service
+
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface CyrptoAPI {
+    @GET("/pricemultifull")
+    suspend fun getCyrptoList(
+        @Query("fsyms") fromSymbols: String = "BTC,ETH,LINK,EOS,BCH,LTC,XRP,OXT,B5V,TRX,XTZ,JST,ETC,SXP,ZEC,ALGO,WAVES,BAND,ADA,XLM,DOGE",
+        @Query("tsyms") toSymbols: String = "USD"
+    )
+}
