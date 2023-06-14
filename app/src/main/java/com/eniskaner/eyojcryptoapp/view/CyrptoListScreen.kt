516a296d6f090d937d084c64d0cb9144ac23156f
Enis <enis.d.kaner@gmail.com>
2023-06-14 13:50:46 +0300

    SerachBar added and Color, Shape, Theme changed for CyrptoListScreen

--- app/src/main/java/com/eniskaner/eyojcryptoapp/view/CyrptoListScreen.kt
+++ app/src/main/java/com/eniskaner/eyojcryptoapp/view/CyrptoListScreen.kt
@@ -1,10 +1,22 @@
 package com.eniskaner.eyojcryptoapp.view
 
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.eniskaner.eyojcryptoapp.viewmodel.CyrptoListViewModel
@@ -15,9 +27,77 @@ fun CyrptoListScreen(
     viewModel: CyrptoListViewModel = hiltViewModel()
 ) {
     Surface(
-        color = MaterialTheme.colorScheme.secondary,
+        color = MaterialTheme.colors.secondary,
         modifier = Modifier.fillMaxSize()
     ) {
+        
+        Column {
+            Text(text = "EyojCyrptoApp",
+                Modifier
+                    .fillMaxWidth()
+                    .padding(20.dp),
+                textAlign = TextAlign.Center,
+                fontSize = 40.sp,
+                fontWeight = FontWeight.Bold,
+                color = MaterialTheme.colors.primary
+            )
+            
+            Spacer(modifier = Modifier.height(10.dp))
+            //Seach
+            SearchBar(
+                hint = "Search ...",
+                modifier = Modifier.fillMaxWidth().padding(16.dp)
+            ) {
+                //viewModel.searchCyrptoList(it)
+            }
+            Spacer(modifier = Modifier.height(10.dp))
+            //List
+        }
 
     }
+}
+
+@Composable
+fun SearchBar(
+    modifier: Modifier = Modifier,
+    hint: String = "",
+    onSearch : (String) -> Unit = {}
+) {
+
+    var text by remember {
+        mutableStateOf("")
+    }
+
+    var isHintDisplayed by remember {
+        mutableStateOf(hint != "")
+    }
+
+    Box(modifier = modifier) {
+        BasicTextField(value = text, onValueChange = {
+            text = it
+            onSearch(it)
+        },
+            maxLines = 1,
+            singleLine = true,
+            textStyle = TextStyle(color = Color.Black),
+            modifier = Modifier
+                .fillMaxWidth()
+                .shadow(5.dp, CircleShape)
+                .background(Color.White, CircleShape)
+                .padding(horizontal = 20.dp, vertical = 12.dp)
+                .onFocusChanged {
+                    isHintDisplayed = it.isFocused != true && text.isEmpty()
+                }
+        )
+        if(isHintDisplayed) {
+            Text(
+                text = hint,
+                color = Color.LightGray,
+                modifier = Modifier
+                    .padding(horizontal = 20.dp, vertical = 12.dp)
+            )
+        }
+    }
+    
+
 }
