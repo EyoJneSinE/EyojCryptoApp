e608e6aa54f2268fdf3a1bdc9f8627520f36571f
Enis <enis.d.kaner@gmail.com>
2023-07-10 16:31:28 +0300

    CyrptoDetailViewModel changed for new Cyrpto api

--- app/src/main/java/com/eniskaner/eyojcryptoapp/viewmodel/CyrptoDetailViewModel.kt
+++ app/src/main/java/com/eniskaner/eyojcryptoapp/viewmodel/CyrptoDetailViewModel.kt
@@ -1,6 +1,7 @@
 package com.eniskaner.eyojcryptoapp.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.eniskaner.eyojcryptoapp.model.Cyrpto
 import com.eniskaner.eyojcryptoapp.repo.CyrptoRepository
 import com.eniskaner.eyojcryptoapp.util.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -11,7 +12,7 @@ class CyrptoDetailViewModel @Inject constructor(
     private val repository: CyrptoRepository
 ) : ViewModel() {
 
-    suspend fun getCyrpto(fromSymbols: String) : Resource<CyrptoList> {
-        return repository.getCyrpto(fromSymbols)
+    suspend fun getCyrpto(id: String) : Resource<List<Cyrpto>> {
+        return repository.getCyrpto(id)
     }
 }
