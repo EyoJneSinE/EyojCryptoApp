60ac070ed219eb4cd67caa80f3c788b93ab90701
Enis <enis.d.kaner@gmail.com>
2023-07-10 15:56:00 +0300

    CyrptoAPI changed for new Cyrpto api

--- app/src/main/java/com/eniskaner/eyojcryptoapp/service/CyrptoAPI.kt
+++ app/src/main/java/com/eniskaner/eyojcryptoapp/service/CyrptoAPI.kt
@@ -1,22 +1,16 @@
 package com.eniskaner.eyojcryptoapp.service
 
+import com.eniskaner.eyojcryptoapp.util.Constatnts.CYRPTO_ID
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface CyrptoAPI {
 
-    //blockchain/list?api_key=30dc5a2a84148f5c9db0496f6f00e591562003bd496cea3a0e1617d0ddbcd13c
-    @GET("blockchain/list")
-    suspend fun getCyrptoAllList(
-        @Query("api_key") api: String
-    )
+    //https://api.coinpaprika.com/v1/coins
+    @GET(".")
+    suspend fun getCyrptoList()
 
-    //pricemultifull?fsyms=BTC&tsyms=USD&api_key=30dc5a2a84148f5c9db0496f6f00e591562003bd496cea3a0e1617d0ddbcd13c
-
-    @GET("pricemultifull")
-    suspend fun getCyrptoList(
-        @Query("fsyms") fromSymbols: String = "BTC,ETH,LINK,EOS,BCH,LTC,XRP,OXT,B5V,TRX,XTZ,JST,ETC,SXP,ZEC,ALGO,WAVES,BAND,ADA,XLM,DOGE",
-        @Query("tsyms") toSymbols: String = "USD",
-        @Query("api_key") api : String
-    ) : CyrptoList
+    //https://api.coinpaprika.com/v1/coins/btc-bitcoin
+    @GET(CYRPTO_ID)
+    suspend fun getCyrpto()
 }
--- app/src/main/java/com/eniskaner/eyojcryptoapp/util/Constatnts.kt
+++ app/src/main/java/com/eniskaner/eyojcryptoapp/util/Constatnts.kt
@@ -2,6 +2,7 @@ package com.eniskaner.eyojcryptoapp.util
 
 object Constatnts {
 
-    const val BASE_URL = "https://api.coinpaprika.com/v1/"
+    const val BASE_URL = "https://api.coinpaprika.com/v1/coins"
     const val API_KEY = "Replace with your api key"
+    const val CYRPTO_ID = ""
 }
