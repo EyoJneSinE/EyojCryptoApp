311096eed3fa20abcc00642f7bc155d66626861f
Enis <enis.d.kaner@gmail.com>
2023-07-10 15:32:39 +0300

    Base url changed

--- app/src/main/java/com/eniskaner/eyojcryptoapp/util/Constatnts.kt
+++ app/src/main/java/com/eniskaner/eyojcryptoapp/util/Constatnts.kt
@@ -2,7 +2,6 @@ package com.eniskaner.eyojcryptoapp.util
 
 object Constatnts {
 
-    const val API_URL = "https://min-api.cryptocompare.com/data/"
-    const val BASE_URL = "https://www.cryptocompare.com"
+    const val BASE_URL = "https://api.coinpaprika.com/v1/"
     const val API_KEY = "Replace with your api key"
 }
