eed92b82908bce8b3ce2dd9d5a34d4dbcf14a922
Enis <enis.d.kaner@gmail.com>
2023-07-12 17:01:24 +0300

    description can be read much easier

--- app/src/main/java/com/eniskaner/eyojcryptoapp/view/CryptoDetailScreen.kt
+++ app/src/main/java/com/eniskaner/eyojcryptoapp/view/CryptoDetailScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
@@ -30,59 +31,67 @@ fun CyrptoDetailScreen(
     viewModel: CryptoDetailViewModel = hiltViewModel()
 ) {
 
-    val cryptoItem = produceState<Resource<Crypto>>(initialValue = Resource.Loading()) {
+    val cryptoItem by produceState<Resource<Crypto>>(initialValue = Resource.Loading()) {
         value = viewModel.getCyrpto(id)
-    }.value
-
-
+    }
     Box(
         modifier = Modifier
             .fillMaxSize()
             .background(MaterialTheme.colors.secondary),
         contentAlignment = Alignment.Center,
     ) {
-        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
             when (cryptoItem) {
                 is Resource.Success -> {
                     val selectedCrypto = cryptoItem.data
-                    Text(
-                        text = selectedCrypto!!.name,
-                        style = MaterialTheme.typography.h3,
-                        modifier = Modifier.padding(2.dp),
-                        fontWeight = FontWeight.Bold,
-                        color = MaterialTheme.colors.primary,
-                        textAlign = TextAlign.Center
-                    )
-                    Image(
-                        painter = rememberImagePainter(data = selectedCrypto.logo),
-                        contentDescription = selectedCrypto.name,
-                        modifier = Modifier
-                            .padding(16.dp)
-                            .size(200.dp, 200.dp)
-                            .clip(CircleShape)
-                            .border(2.dp, Color.Gray, CircleShape)
-                    )
-                    Text(
-                        text = price,
-                        style = MaterialTheme.typography.h4,
-                        modifier = Modifier.padding(2.dp),
-                        fontWeight = FontWeight.Bold,
-                        color = MaterialTheme.colors.primaryVariant,
-                        textAlign = TextAlign.Center
-                    )
-                    Text(
-                        text = selectedCrypto.description,
-                        style = MaterialTheme.typography.h4,
-                        modifier = Modifier.padding(2.dp),
-                        color = MaterialTheme.colors.primary,
-                        textAlign = TextAlign.Start
-                    )
+                    item {
+                        Text(
+                            text = selectedCrypto!!.name,
+                            style = MaterialTheme.typography.h3,
+                            modifier = Modifier.padding(2.dp),
+                            fontWeight = FontWeight.Bold,
+                            color = MaterialTheme.colors.primary,
+                            textAlign = TextAlign.Center
+                        )
+                        Image(
+                            painter = rememberImagePainter(data = selectedCrypto.logo),
+                            contentDescription = selectedCrypto.name,
+                            modifier = Modifier
+                                .padding(16.dp)
+                                .size(200.dp, 200.dp)
+                                .clip(CircleShape)
+                                .border(2.dp, Color.Gray, CircleShape)
+                        )
+                        Text(
+                            text = price,
+                            style = MaterialTheme.typography.h4,
+                            modifier = Modifier.padding(2.dp),
+                            fontWeight = FontWeight.Bold,
+                            color = MaterialTheme.colors.primaryVariant,
+                            textAlign = TextAlign.Center
+                        )
+                        Text(
+                            text = selectedCrypto.description,
+                            style = MaterialTheme.typography.h4,
+                            modifier = Modifier.padding(2.dp),
+                            color = MaterialTheme.colors.primary,
+                            textAlign = TextAlign.Start
+                        )
+                    }
                 }
                 is Resource.Error -> {
-                    Text(cryptoItem.message!!)
+                    item {
+                        Text(cryptoItem.message!!)
+                    }
                 }
                 is Resource.Loading -> {
-                    CircularProgressIndicator(color = MaterialTheme.colors.primary)
+                    item {
+                        CircularProgressIndicator(color = MaterialTheme.colors.primary)
+                    }
                 }
             }
         }
